3537dc125f56ee9c2f5ea2a6bf3c39a6be1bf73a
Anthony Nguyen <antnguye@umich.edu>
2023-04-03 12:29:32 -0400

    updated file path for normalcy table for test

--- qm2_human_rarity/tests/test_compare_1000_genomes.py
+++ qm2_human_rarity/tests/test_compare_1000_genomes.py
@@ -8,7 +8,7 @@ final_dels = compare_against_1000.find_deletions(output_table, output_dict)
 
 
 def test_compare_1000_genomes():
-    normal_file_path = "../99_normalcy_range_tenk_genomes.npy"
+    normal_file_path = "qm2_human_rarity/99_normalcy_range_tenk_genomes.npy"
     normal_dups, normal_dels = compare_against_1000.compare_1000_genomes(output_dict, normal_file_path, final_dups, final_dels)
     assert normal_dups['chr1:123494757-123585956'] is True, "This duplication should be rare."
     assert normal_dels['chr1:6378151-6385794'] is True, "This deletion should be rare."
