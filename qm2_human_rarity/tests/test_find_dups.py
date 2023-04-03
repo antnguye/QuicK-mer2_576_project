d384560754d81b32eeee23445476e74aa4d0dc98
Anthony Nguyen <antnguye@umich.edu>
2023-04-03 12:54:49 -0400

    updated test formatting for linter

--- qm2_human_rarity/tests/test_find_dups.py
+++ qm2_human_rarity/tests/test_find_dups.py
@@ -9,7 +9,8 @@ def test_find_dups():
         test_chro_dict[i] = ["chr1", 0, 1]
     test_answer = [[[3, 2], [4, 3], [5, 4], [6, 5]]]
     dup_windows = compare_against_1000.find_dups(test_array, test_chro_dict)
-    assert test_answer == dup_windows, "The correct test array is [[[3, 2], [4, 3], [5, 4], [6, 5]]]."
+    assert test_answer == dup_windows, "The correct test array is [[[3, 2], [4, 3], " \
+                                       "[5, 4], [6, 5]]]."
 
 
 def test_find_dups_chro_break_and_end():
@@ -22,7 +23,8 @@ def test_find_dups_chro_break_and_end():
             test_chro_dict[i] = ["chr2", 0, 1]
     test_answer = [[[3, 2], [4, 3], [5, 4], [6, 5]], [[4, 14], [5, 15], [6, 16]]]
     dup_windows = compare_against_1000.find_dups(test_array, test_chro_dict)
-    assert test_answer == dup_windows, "The correct test array is [[[3, 2], [4, 3], [5, 4], [6, 5]], [[4, 14], [5, 15], [6, 16]]]."
+    assert test_answer == dup_windows, "The correct test array is [[[3, 2], [4, 3], " \
+                                       "[5, 4], [6, 5]], [[4, 14], [5, 15], [6, 16]]]."
 
 
 def test_find_dups_none_found():
@@ -41,4 +43,5 @@ def test_find_dups_empty():
     for i in range(13):
         test_chro_dict[i] = ["chr1", 0, 1]
     dup_windows = compare_against_1000.find_dups(None, test_chro_dict)
-    assert test_answer == dup_windows, "There should be no table provided in this test."
+    assert test_answer == dup_windows, "There should be no table provided in this " \
+                                       "test."
